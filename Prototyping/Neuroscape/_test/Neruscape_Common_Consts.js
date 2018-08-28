359c2558428578fcfa589f09989a5aae48da187e
milad <milad@miladnazeri.com>
2018-08-28 12:33:55 -0700

    adding in test folder

---
+++ Prototyping/Neuroscape/_test/Neruscape_Common_Consts.js
@@ -0,0 +1,15 @@
+var BASE_NAME = "Neuroscape_";
+
+module.exports = {
+    ORB: BASE_NAME + "Orb",
+    STICK_LEFT: BASE_NAME + "Drum_Stick_Left",
+    STICK_RIGHT: BASE_NAME + "Drum_Stick_Right",
+    PAD_LEFT: BASE_NAME + "Drum_Pad_Right",
+    PAD_RIGHT: BASE_NAME + "Drum_Pad_Right",
+    BOUNDARY_LEFT: BASE_NAME + "Boundary_Left",
+    BOUNDARY_LEFT_TOP: BASE_NAME + "Boundary_Left_Top",
+    BOUNDARY_LEFT_BOTTOM: BASE_NAME + "Boundary_Left_Bottom",
+    BOUNDARY_RIGHT: BASE_NAME + "Boundary_Right",
+    BOUNDARY_RIGHT_TOP: BASE_NAME + "Boundary_Right_Top",
+    BOUNDARY_RIGHT_BOTTOM: BASE_NAME + "Boundary_Right_Bottom"
+};
